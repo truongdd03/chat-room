cca805ea326922e9e9f94c1af16b987379341a11
Don Truong <dong23102003@gmail.com>
2023-02-24 00:27:11 -0500

    add homepage

--- ChatClient/src/main.ts
+++ ChatClient/src/main.ts
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+import "bulma/css/bulma.css";
 import './assets/main.css'
 
 const app = createApp(App)
--- ChatClient/src/views/HomeView.vue
+++ ChatClient/src/views/HomeView.vue
@@ -1,9 +1,26 @@
+<template>
+  <div class="wrapper">
+    <input class="input has-background-dark	is-primary has-text-white is-large" placeholder="Enter your name...">
+    <p class="button is-primary is-large ml-2">Join!</p>
+  </div>
+</template>
+
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
-<template>
-  <main>
-    <TheWelcome />
-  </main>
-</template>
+<style scoped>
+.wrapper {
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.input {
+  width: 50vw;
+}
+
+.button {
+  width: 200px;
+}
+</style>
